a9e65386f56b981de5ca7c29150e0f7e67e93b54
FLlorente <ferllorente.93@gmail.com>
2024-07-04 13:17:24 -0400

    added options to train_stacking for parallel/sequential, showing progress and summary

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ from modules.data_handling import load_and_normalize_data, split_dataset, create
 from modules.prediction_storage import store_predictions
 from modules.fusion_methods import compute_neg_log_like, product_fusion, train_stacking, predict_stacking, train_and_predict_fusion_method
 from modules.model_training import train_and_predict_single_gp, train_expert, store_predictions_for_experts
-from modules.phs import phs
+from modules.phs import phs, phs_with_normalized_w
 from modules.bhs import bhs
 
 # Parameters
 dataset_name = 'concrete'  
 # dataset_name = 'yacht'  
+# dataset_name = 'elevators'
 split = 0
-n_experts = 50
-n_points_per_split = 1
+n_experts = 2
+n_points_per_split = 2
 kappa = 2    # noise = np.var(y_train)/kappa**2  ; kappa \in [2,100]
 lambdaa = 1   # lengthscale = np.std(X_train,1)/lambdaa ; lambdaa \in [1,10]
 
@@ -33,7 +34,8 @@ X_val, y_val = create_validation_set(splits, n_points_per_split)
 
 # ---------  Single GP using all training data ----- #
 test_preds, _ = train_and_predict_single_gp(X_train, y_train, X_test, X_val, kappa, lambdaa)
-nlpd_single_gp = compute_neg_log_like(test_preds.mean.numpy().reshape(-1, 1), np.sqrt(test_preds.variance.numpy().reshape(-1, 1)), y_test)
+nlpd_single_gp = compute_neg_log_like(test_preds.mean.numpy().reshape(-1, 1), 
+                                      np.sqrt(test_preds.variance.numpy().reshape(-1, 1)), y_test)
 
 
 # --------------- experts --------------- #
@@ -85,6 +87,8 @@ samples_phs = train_stacking(
     mu_preds_val=mu_preds_val,
     std_preds_val=std_preds_val,
     y_val=y_val,
+    # parallel = True,
+    show_progress=True,
 )
 
 preds_phs, lpd_phs_test = predict_stacking(
@@ -112,6 +116,23 @@ preds_phs, lpd_phs_test = predict_stacking(
 
 print("NLPD PHS: ", -lpd_phs_test.mean())
 # print("NLPD PHS: ", -lpd_phs_test_otro.mean())
+
+
+
+# # ------- phs with normalized weights ---- #
+
+# preds_phs_norm, lpd_phs_norm_test = train_and_predict_fusion_method(  # this is the NEW way
+#             model=phs_with_normalized_w,
+#             X_val=X_val,
+#             mu_preds_val=mu_preds_val,
+#             std_preds_val=std_preds_val,
+#             y_val=y_val,
+#             X_test=X_test,
+#             mu_preds_test=mu_preds_test,
+#             std_preds_test=std_preds_test,
+#             y_test=y_test
+#         )
+# print("NLPD PHS-norm: ", -lpd_phs_norm_test.mean())
 
 
 
@@ -153,31 +174,39 @@ print("NLPD BHS: ", -lpd_bhs_test.mean())
 
 
 
-plt.figure()
-for i in range(w_gpoe.shape[1]):
-    plt.plot(w_gpoe[:, i])
-plt.title("gPoEs")
+# plt.figure()
+# for i in range(w_gpoe.shape[1]):
+#     plt.plot(w_gpoe[:, i])
+# plt.title("gPoEs")
 
-plt.figure()
-for i in range(w_unif.shape[1]):
-    plt.plot(w_unif[:, i])
-plt.title("Unif-gPoEs")
+# plt.figure()
+# for i in range(w_unif.shape[1]):
+#     plt.plot(w_unif[:, i])
+# plt.title("Unif-gPoEs")
+# print(w_unif.sum(1))
 
 
+# plt.figure()
+# for i in range(n_experts):
+#     plt.plot(preds_phs["w"].mean(0)[i,:])
+# # plt.show()
+# plt.title("PHS") 
 
-plt.figure()
-for i in range(n_experts):
-    plt.plot(preds_phs["w"].mean(0)[i,:])
+# plt.figure()
+# for i in range(n_experts):
+#     plt.plot(preds_phs_norm["w"].mean(0)[i,:])
+# # plt.show()
+# plt.title("PHS-norm") 
+
+# print(preds_phs_norm["w"].mean(0).sum(1))
+
+
+# plt.figure()
+# for i in range(n_experts):
+#     plt.plot(preds_bhs["w"].mean(0)[:, i])
+# plt.title("BHS")
+
+
 # plt.show()
-plt.title("PHS") 
-
-
-plt.figure()
-for i in range(n_experts):
-    plt.plot(preds_bhs["w"].mean(0)[:, i])
-plt.title("BHS")
-
-
-plt.show()
 
 
