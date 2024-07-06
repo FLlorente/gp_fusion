2e50b01ec44d1db724848030d56175248b2015b6
FLlorente <ferllorente.93@gmail.com>
2024-07-05 20:11:22 -0400

    added expert construction and stacking to main_tall_data

--- main_tall_data.py
+++ main_tall_data.py
@@ -16,10 +16,14 @@ from modules.model_training import train_joint_experts_shared_kernel
 from modules.model_training import train_variational_gp, predict_variational_gp
 
 # Parameters
-# dataset_name = 'concrete'  
-# dataset_name = 'yacht'  
-dataset_name = 'elevators'
+dataset_name = 'concrete'  
+# dataset_name = 'elevators'
 # dataset_name = "airfoil"
+# dataset_name = "protein"
+# dataset_name = "kin40k"
+# dataset_name = "parkinsons"
+
+
 split = 1
 n_experts = 5
 n_points_per_split = 1
@@ -32,38 +36,137 @@ num_epochs = 10
 # ------------ Load and normalize data --------- #
 X_train, y_train, X_test, y_test = load_and_normalize_data(dataset_name, split)
 
+print("training size: ", len(y_train))
+print("test size: ", len(y_test))
+
+# # ---------  Exact GP using all training data ----- #
+# test_preds, _ = train_and_predict_single_gp(X_train, y_train, X_test, X_test, 
+#                                             kappa, lambdaa,
+#                                             lr=lr,
+#                                             training_iter=training_iter)
+# nlpd_exact_gp = compute_neg_log_like(test_preds.mean.numpy().reshape(-1, 1), 
+#                                       np.sqrt(test_preds.variance.numpy().reshape(-1, 1)), y_test)
+# # Output results
+# print("NLPD Exact GP: ", nlpd_exact_gp)
 
 
-# ---------  Exact GP using all training data ----- #
-test_preds, _ = train_and_predict_single_gp(X_train, y_train, X_test, X_test, 
-                                            kappa, lambdaa,
-                                            lr=lr,
-                                            training_iter=training_iter)
-nlpd_exact_gp = compute_neg_log_like(test_preds.mean.numpy().reshape(-1, 1), 
-                                      np.sqrt(test_preds.variance.numpy().reshape(-1, 1)), y_test)
-# Output results
-print("NLPD Exact GP: ", nlpd_exact_gp)
-
-# --------- Variational GP -------- #
-
-# Use a subset of the training points as inducing points
+# ------- variational GP with "large" number of inducing points ------- #
 np.random.seed(0)
-inducing_points = X_train[np.random.choice(X_train.shape[0], 100, replace=False), :]
+inducing_points = X_train[np.random.choice(X_train.shape[0], 300, replace=False), :]
 
 # Train the Variational GP with mini-batch
 model, likelihood = train_variational_gp(X_train, y_train, 
-                                         inducing_points, 
-                                         kappa=kappa,lambdaa=lambdaa,
-                                         learning_rate=lr,
-                                         num_epochs=num_epochs,
-                                         )
+                                        inducing_points, 
+                                        kappa=kappa,lambdaa=lambdaa,
+                                        learning_rate=lr,
+                                        num_epochs=num_epochs,
+                                        )
 
 # Predict on test data
 mean, std = predict_variational_gp(model, likelihood, X_test)
-
 nlpd_var_gp = compute_neg_log_like(mean.reshape(-1,1),std.reshape(-1,1),y_test)
 # Output results
-print("NLPD Variational GP: ", nlpd_var_gp)
+print("NLPD Variational GP (more ind points): ", nlpd_var_gp)
+
+# --------- Variational GP Experts with "small" number of inducing points -------- #
+indices = np.random.choice(X_train.shape[0], 100, replace=False)
+X_val = X_train[indices, :]
+y_val = y_train[indices]
+#
+mu_preds_test,std_preds_test = [], []
+mu_preds_val,std_preds_val = [], []
+for i in range(3):
+    np.random.seed(i)
+    inducing_points = X_train[np.random.choice(X_train.shape[0], 100, replace=False), :]
+
+    # Train the Variational GP with mini-batch
+    model, likelihood = train_variational_gp(X_train, y_train, 
+                                            inducing_points, 
+                                            kappa=kappa,lambdaa=lambdaa,
+                                            learning_rate=lr,
+                                            num_epochs=num_epochs,
+                                            )
+
+    # Predict on test data
+    mean, std = predict_variational_gp(model, likelihood, X_test)
+
+    mu_preds_test.append(mean)
+    std_preds_test.append(std)
+
+    # Predict on validation data
+    mean, std = predict_variational_gp(model, likelihood, X_val)
+
+    mu_preds_val.append(mean)
+    std_preds_val.append(std)
+
+mu_preds_test = np.stack(mu_preds_test,axis=-1)
+std_preds_test = np.stack(std_preds_test,axis=-1)
+mu_preds_val = np.stack(mu_preds_val,axis=-1)
+std_preds_val = np.stack(std_preds_val,axis=-1)
+
+nlpd_var_gp_ex = compute_neg_log_like(mu_preds_test,std_preds_test,y_test)
+# Output results
+print("NLPD Variational GP Experts: ", nlpd_var_gp_ex)
 
 
+# ------- Bayesian stacking algorithms ----- #
+# ---------- PHS training and prediction --------- #
+samples_phs = train_stacking(
+    model=phs,
+    X_val=X_val,
+    mu_preds_val=mu_preds_val,
+    std_preds_val=std_preds_val,
+    y_val=y_val,
+    parallel = True,
+    show_progress=True,
+)
+preds_phs, lpd_phs_test = predict_stacking(
+    model=phs,
+    samples=samples_phs,
+    X_val=X_val,
+    X_test=X_test,
+    mu_preds_test=mu_preds_test,
+    std_preds_test=std_preds_test,
+    y_test=y_test,
+    prior_mean=lambda x: -np.log(mu_preds_test.shape[1]) * np.ones(x.shape[0]),
+)
+print("NLPD PHS: ", -lpd_phs_test.mean())
 
+# ------------  BHS training and prediction -------- #
+samples_bhs = train_stacking(
+    model=bhs,
+    X_val=X_val,
+    mu_preds_val=mu_preds_val,
+    std_preds_val=std_preds_val,
+    y_val=y_val,
+    parallel = True,
+    show_progress=True,
+)
+preds_bhs, lpd_bhs_test = predict_stacking(
+    model=bhs,
+    samples=samples_bhs,
+    X_val=X_val,
+    X_test=X_test,
+    mu_preds_test=mu_preds_test,
+    std_preds_test=std_preds_test,
+    y_test=y_test,
+    prior_mean=lambda x: -np.log(mu_preds_test.shape[1]) * np.ones(x.shape[0]),
+)
+print("NLPD BHS: ", -lpd_bhs_test.mean())
+
+
+plot=1
+if plot:
+    plt.figure()
+    for i in range(n_experts):
+        plt.plot(preds_phs["w"].mean(0)[i,:])
+    # plt.show()
+    plt.title("PHS") 
+
+
+    plt.figure()
+    for i in range(n_experts):
+        plt.plot(preds_bhs["w"].mean(0)[:, i])
+    plt.title("BHS")
+
+    plt.show()
